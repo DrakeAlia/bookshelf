59ae798f8c67b688a32ec57a4dd1439aa8677591
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-09-10 11:39:22 -0700

    completed List item List and Discover Screen List (Extra)

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -4,6 +4,7 @@ import {jsx} from '@emotion/core'
 import {useListItems} from 'utils/list-items'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
+import {Profiler} from './profiler'
 
 function ListItemList({filterListItems, noListItems, noFilteredListItems}) {
   const listItems = useListItems()
@@ -22,14 +23,37 @@ function ListItemList({filterListItems, noListItems, noFilteredListItems}) {
   }
 
   return (
-    <BookListUL>
-      {filteredListItems.map(listItem => (
-        <li key={listItem.id} aria-label={listItem.book.title}>
-          <BookRow book={listItem.book} />
-        </li>
-      ))}
-    </BookListUL>
+    <Profiler
+      id="List Item List"
+      metadata={{listItemCount: filteredListItems.length}}
+    >
+      <BookListUL>
+        {filteredListItems.map(listItem => (
+          <li key={listItem.id} aria-label={listItem.book.title}>
+            <BookRow book={listItem.book} />
+          </li>
+        ))}
+      </BookListUL>
+    </Profiler>
   )
 }
 
 export {ListItemList}
+
+// List item List and Discover Screen List (Extra) ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Let's add some profiling to this list of books on both of these pages, and also some profiling to these books here 
+// as well. Both of those listItem pages use this listItem list component so we can just wrap our booklist ul in a 
+// profiler.
+
+// Here, we'll render the profiler and we'll put our booklist ul inside of that. Of course, we will need to import 
+// the profiler. We'll import { profiler } from 'profiler'. We're in the component's directory here, so it's right 
+// next door.
+
+// In review, all that we did here was apply the profiler to two other places in our app. You typically want to put 
+// the profiler in very specific parts of your app that you have a feeling might run into a performance regression of 
+// some kind.
+
+// The reason that you'll do it localized like we're doing here, rather than just globally around your whole app is 
+// because the more localized you are, the more information that you can give to your backend for the graphs that you 
+// show to help in the triaging of performance regressions.
