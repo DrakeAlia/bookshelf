dd762b3e4133e2468230b60a10890fe310992b71
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-09-10 19:10:31 -0700

    completed Preload All Initial Data (Extra)

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -2,6 +2,7 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
+import { queryCache } from 'react-query'
 import * as auth from 'auth-provider'
 import {client} from 'utils/api-client'
 import {useAsync} from 'utils/hooks'
@@ -13,7 +14,10 @@ async function getUser() {
 
   const token = await auth.getToken()
   if (token) {
-    const data = await client('me', {token})
+    const data = await client('bootstrap', {token})
+    queryCache.setQueryData('list-items', data.listItems, {
+      staleTime: 5000,
+    })
     user = data.user
   }
 
@@ -105,7 +109,7 @@ function useClient() {
 
 export {AuthProvider, useAuth, useClient}
 
-// Fetch User before AuthProvider Mounts /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Fetch User before AuthProvider Mounts ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Let's take a look at the problem here first. If I right click here with the DevTools open, I'll see this empty 
 // cache and hard reload. Let's try that.
@@ -116,3 +120,22 @@ export {AuthProvider, useAuth, useClient}
 // Literally all that we did is we took this one line, which was called after the authProvider's mounted. We moved 
 // it out of the authProvider entirely, so that it's called at the same time the authProvider is defined, giving us 
 // a little bit of time to start getting the user's information when the app is loading.
+
+// Preload All Initial Data (Extra) /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// For the extra credit we've got another little optimization that we can make here, and that is we get the user's 
+// information. Then we wait until that user's information comes back before we go to get the user's list items. 
+// We have a waterfall effect between those two requests.
+
+// It would be better if we make both of those requests at the same time, because if we have the user's token and 
+// they're logged in, we may as well get the user's username while we're getting all of the user's list items.
+
+// In review, what we did here is we updated our getUser to call the bootstrap endpoint instead, which will not only 
+// give us the user but also give the user's list items, as well.
+
+// Then we can use that to set that into the queryCache, so that any component that uses those list items will have 
+// immediate access to them, rather than having to request them.
+
+// Then we prevented react-query from attempting to update that cache for five seconds, so that our components have 
+// time to render and display that data without react-query bothering to update that data, without react-query 
+// automatically assuming that data is stale. 
