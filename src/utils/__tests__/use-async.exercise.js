360363f7222213898367348346f107149214a10e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-09-14 18:29:12 -0700

    completed Set up useAsync Test with renderHook

--- src/utils/__tests__/use-async.exercise.js
+++ src/utils/__tests__/use-async.exercise.js
@@ -1,7 +1,7 @@
-// 🐨 instead of React Testing Library, you'll use React Hooks Testing Library
-// import {renderHook, act} from '@testing-library/react-hooks'
+// 🐨 instead of React Testing Library, you'll use React Hooks Testing Library (X)
+import {renderHook, act} from '@testing-library/react-hooks'
 // 🐨 Here's the thing you'll be testing:
-// import {useAsync} from '../hooks'
+import {useAsync} from '../hooks'
 
 // 💰 I'm going to give this to you. It's a way for you to create a promise
 // which you can imperatively resolve or reject whenever you want.
@@ -23,7 +23,22 @@
 // do stuff/make assertions you want to after the promise has resolved
 
 // 🐨 flesh out these tests
-test.todo('calling run with a promise which resolves')
+test('calling run with a promise which resolves', async () => {
+  const {result} = renderHook(() => useAsync())
+  expect(result.current).toEqual({
+    isIdle: true,
+    isLoading: false,
+    isError: false,
+    isSuccess: false,
+    setData: expect.any(Function),
+    setError: expect.any(Function),
+    error: null,
+    status: 'idle',
+    data: null,
+    run: expect.any(Function),
+    reset: expect.any(Function),
+  })
+})
 // 🐨 get a promise and resolve function from the deferred utility
 // 🐨 use renderHook with useAsync to get the result
 // 🐨 assert the result.current is the correct default state
@@ -39,23 +54,38 @@ test.todo('calling run with a promise which resolves')
 // 🐨 call `reset` (💰 this will update state, so...)
 // 🐨 assert the result.current has actually been reset
 
-test.todo('calling run with a promise which rejects')
+test('calling run with a promise which rejects', async () => {})
 // 🐨 this will be very similar to the previous test, except you'll reject the
 // promise instead and assert on the error state.
 // 💰 to avoid the promise actually failing your test, you can catch
 //    the promise returned from `run` with `.catch(() => {})`
 
-test.todo('can specify an initial state')
+test('can specify an initial state', async () => {})
 // 💰 useAsync(customInitialState)
 
-test.todo('can set the data')
+test('can set the data', async () => {})
 // 💰 result.current.setData('whatever you want')
 
-test.todo('can set the error')
+test('can set the error', async () => {})
 // 💰 result.current.setError('whatever you want')
 
-test.todo('No state updates happen if the component is unmounted while pending')
+test('No state updates happen if the component is unmounted while pending', async () => {})
 // 💰 const {result, unmount} = renderHook(...)
 // 🐨 ensure that console.error is not called (React will call console.error if updates happen when unmounted)
 
-test.todo('calling "run" without a promise results in an early error')
+test('calling "run" without a promise results in an early error', async () => {})
+
+// Set up useAsync Test with renderHook ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// It looks like this useAsync hook has a lot to do, so let's do it. Let's get rid of the todos. We'll make an 
+// async test here because we are testing something asynchronous. We'll probably need to await some stuff here.
+
+// What we did here is, we called useAsync. When we called it by itself, we realized that we can't do that 
+// because that can only happen within the body of a function component when React is rendering that function 
+// component.
+
+// We're using react-hooks-testing-library and the renderHook method from it to manage creating a function 
+// component and calling our useAsync hook here within the function body of that function component.
+
+// That returns a couple utilities, as well as the result, which is whatever we return from useAsync. Then, we 
+// can assert what we get back.
