44682c85f617b220bc0b47d111c974813d134e59
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-09-14 19:47:56 -0700

    Wrap an act around an async Fucntion completed

--- src/utils/__tests__/use-async.exercise.js
+++ src/utils/__tests__/use-async.exercise.js
@@ -4,7 +4,7 @@ import {renderHook, act} from '@testing-library/react-hooks'
 import {useAsync} from '../hooks'
 
 // 💰 I'm going to give this to you. It's a way for you to create a promise
-// which you can imperatively resolve or reject whenever you want.
+// which you can imperatively resolve or reject whenever you want. (X)
 // function deferred() {
 //   let resolve, reject
 //   const promise = new Promise((res, rej) => {
@@ -23,7 +23,24 @@ import {useAsync} from '../hooks'
 // do stuff/make assertions you want to after the promise has resolved
 
 // 🐨 flesh out these tests
+
+// 🐨 get a promise and resolve function from the deferred utility (X)
+// 🐨 use renderHook with useAsync to get the result (X)
+// 🐨 assert the result.current is the correct default state (X)
+
+// 🐨 call `run`, passing the promise (X)
+//    (💰 this updates state so it needs to be done in an `act` callback)
+// 🐨 assert that result.current is the correct pending state (X)
+
+// 🐨 call resolve and wait for the promise to be resolved
+//    (💰 this updates state too and you'll need it to be an async `act` call so you can await the promise)
+// 🐨 assert the resolved state
 test('calling run with a promise which resolves', async () => {
+  let resolve, reject
+  const promise = new Promise((res, rej) => {
+    resolve = res
+    reject = rej
+  })
   const {result} = renderHook(() => useAsync())
   expect(result.current).toEqual({
     isIdle: true,
@@ -38,18 +55,28 @@ test('calling run with a promise which resolves', async () => {
     run: expect.any(Function),
     reset: expect.any(Function),
   })
+
+  act(() => {
+    result.current.run(promise)
+  })
+
+  expect(result.current).toEqual({
+    isIdle: false,
+    isLoading: true,
+    isError: false,
+    isSuccess: false,
+    setData: expect.any(Function),
+    setError: expect.any(Function),
+    error: null,
+    status: 'pending',
+    data: null,
+    run: expect.any(Function),
+    reset: expect.any(Function),
+  })
 })
-// 🐨 get a promise and resolve function from the deferred utility
-// 🐨 use renderHook with useAsync to get the result
-// 🐨 assert the result.current is the correct default state
 
-// 🐨 call `run`, passing the promise
-//    (💰 this updates state so it needs to be done in an `act` callback)
-// 🐨 assert that result.current is the correct pending state
 
-// 🐨 call resolve and wait for the promise to be resolved
-//    (💰 this updates state too and you'll need it to be an async `act` call so you can await the promise)
-// 🐨 assert the resolved state
+
 
 // 🐨 call `reset` (💰 this will update state, so...)
 // 🐨 assert the result.current has actually been reset
@@ -77,15 +104,36 @@ test('calling "run" without a promise results in an early error', async () => {}
 
 // Set up useAsync Test with renderHook ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// It looks like this useAsync hook has a lot to do, so let's do it. Let's get rid of the todos. We'll make an 
+// It looks like this useAsync hook has a lot to do, so let's do it. Let's get rid of the todos. We'll make an
 // async test here because we are testing something asynchronous. We'll probably need to await some stuff here.
 
-// What we did here is, we called useAsync. When we called it by itself, we realized that we can't do that 
-// because that can only happen within the body of a function component when React is rendering that function 
+// What we did here is, we called useAsync. When we called it by itself, we realized that we can't do that
+// because that can only happen within the body of a function component when React is rendering that function
 // component.
 
-// We're using react-hooks-testing-library and the renderHook method from it to manage creating a function 
+// We're using react-hooks-testing-library and the renderHook method from it to manage creating a function
 // component and calling our useAsync hook here within the function body of that function component.
 
-// That returns a couple utilities, as well as the result, which is whatever we return from useAsync. Then, we 
+// That returns a couple utilities, as well as the result, which is whatever we return from useAsync. Then, we
 // can assert what we get back.
+
+// Wrap an act around an async Fucntion /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// All right, so I've organized some of these values here to make it a little bit easier. The next thing we want to
+// do is call this run function, and make sure that the state update's appropriately from that.
+
+// We'll say result.current.run, and we'll call that. Now we're going to get an error that says, "Hey, the
+// argument passed to useAsync.run must be a promise," and we didn't pass anything. Let's make a promise to
+// handle this for us.
+
+// Now we're one step closer where we call the run function, and we can assert on all of the change state, because
+// of that run function being called.
+
+// We also noticed an act warning, and because we're triggering a state update directly when we call this run
+// function we need to tell React that, yes, we are expecting this to happen. We want all of those effects to be
+// flushed when we finish running all of this stuff we're doing inside this callback.
+
+// This ensures that we're asserting the same UI type that the end-user would be interacting with when we make our
+// assertions.
+
+// Add an async act to Resolve a Promise
